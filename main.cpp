d46fed66b2bbda58ecd93709d5e066ab5f6295f6
Andrei Verevkin <321@.com>
2015-09-16 15:39:16 +0300

    Array, circle

--- main.cpp
+++ main.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    cout << "hello" <<endl;
+
+    int arr[9];
+    int n;
+    //arr[0]={3};
+    for (n=0; n<=9; n++)
+    {
+    cout << arr[n]<<endl;
+
+    }
+
+
+    //cout << "hello" <<endl;
     return 0;
 }
