d4a4a0273c49a234d1f42550c42cad14ab219991
Andrei Verevkin <321@.com>
2015-10-14 15:31:43 +0300

    reshenie sinx=ax

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-void print (int x[], int N)
+/*void print (int x[], int N)
 {
     for (int n=0; n<=N; n++)
         cout << x[n] <<endl;
@@ -30,4 +31,65 @@ int main(int argc, char *argv[])
 
     //cout << "hello" <<endl;
     return 0;
+}*/
+
+double f(double x)
+{
+    const double alpha = 0.1;
+    return sin(x) - alpha*x;
 }
+
+
+double dichotomy (
+        double x1,
+        double x2,
+        double tolerance
+        )
+{
+    while (fabs(x2-x1)>tolerance)
+   {
+    double x3=(x1+x2)/2;
+    if (f(x3)*f(x2)>0)
+        x2=x3;
+    else
+        x1=x3;
+    }
+
+
+    return x2;
+}
+
+
+int main(int argc, char *argv[])
+{
+    double pi = 2*asin(1);
+    cout << dichotomy(pi/2, 3*pi/2, 0.001) << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
