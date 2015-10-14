ec8ee3a6112445cabecbf2cd9e01e1ea9075ec95
Andrei Verevkin <321@.com>
2015-10-14 14:21:12 +0300

    functions

--- main.cpp
+++ main.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
 using namespace std;
 
+void print (int x[], int N)
+{
+    for (int n=0; n<=N; n++)
+        cout << x[n] <<endl;
+}
+
 int main(int argc, char *argv[])
 {
+    const int N=9;
+    int arr[N];
 
-    int arr[9];
-    int n;
-    //arr[0]={3};
-    for (n=0; n<=9; n++)
-    {
-    cout << arr[n]<<endl;
 
-    }
+     for (int n=0; n<=N; ++n)
+         arr[n]=n+1;
+
+    print (arr, N);
+
+
+    for (int n=0; n<=N; n++)
+        arr[n]=arr[n]*arr[n];
+
+
+    for (int n=0; n<=N; n++)
+        cout << arr[n]<<endl;
+
 
 
     //cout << "hello" <<endl;
